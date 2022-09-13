82a9ff7ccd467bba8a1c04c27d19493d989a7f10
edu3598 <edu3598@DESKTOP-QEHCVVJ.mshome.net>
2022-09-13 15:17:23 +0900

    카카오페이 결제창 생성

--- pt_pro/src/main/java/com/project/pt/pay/controller/PayController.java
+++ pt_pro/src/main/java/com/project/pt/pay/controller/PayController.java
@@ -1,5 +1,15 @@
 package com.project.pt.pay.controller;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -9,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.pt.pay.service.PayService;
 import com.project.pt.pay.vo.PayVO;
@@ -16,27 +27,71 @@ import com.project.pt.pay.vo.PayVO;
 @Controller
 public class PayController {
 	private static final Logger logger = LoggerFactory.getLogger(PayController.class);
-	
+
 	@Autowired
 	private PayService service;
-	
+
 	@RequestMapping(value="/pay", method=RequestMethod.GET)
 	public String pay(HttpSession session) {
-		
+
 		return "/pay";
-		
+
 	}
-	
+
 	@RequestMapping(value="/pay", method=RequestMethod.POST)
 	public String pay(PayVO pv, HttpSession session) {
 		boolean result = service.modify(pv);
-		
+
 		if(result) {
-			return "/pay/카카오페이결제창";
+			return "/pay";
 		} else {
 			return "redirect:/";
 		}
 	}
-	
+
+	@RequestMapping(value="/kakaopay")
+	@ResponseBody
+	public String kakaopay() {
+		try {
+			URL address = new URL("https://kapi.kakao.com//v1/payment/ready");
+			HttpURLConnection serverConnection = (HttpURLConnection) address.openConnection();
+			serverConnection.setRequestMethod("POST");
+			serverConnection.setRequestProperty("Authorization", " KakaoAK 	972abc86e272474955b5831e6ceb361b");
+			serverConnection.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+			serverConnection.setDoOutput(true); //연결을 통해서 서버에게 전달해줄게 있으면 true
+			String param = "cid=TC0ONETIME"
+					  + "partner_order_id=partner_order_id"
+					  + "partner_user_id=partner_user_id"
+					  + "item_name=pt결제&quantity=1"
+					  + "total_amount=5000"
+					  + "vat_amount=200"
+					  + "tax_free_amount=0"
+					  + "approval_url=https://localhost8080/pt/pay.jsp"
+					  + "fail_url=https://localhost8080/pt/pay.jsp"
+					  + "&cancel_url=https://localhost8080/pt/pay.jsp";
+			OutputStream outputStream = serverConnection.getOutputStream();
+			DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+			dataOutputStream.writeBytes(param);
+			dataOutputStream.close();
+			
+			int result = serverConnection.getResponseCode();
+			
+			InputStream inputStream;
+			if(result == 200) {
+				inputStream = serverConnection.getInputStream();
+			} else {
+				inputStream = serverConnection.getErrorStream();
+			}
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			return bufferedReader.readLine();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "/kakaopay";
+	}
+
 
 }
--- pt_pro/src/main/webapp/WEB-INF/views/pay.jsp
+++ pt_pro/src/main/webapp/WEB-INF/views/pay.jsp
@@ -1,17 +1,54 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
+<script type="text/javascript"
+	src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript"
+	src="https://service.iamport.kr/js/iamport.payment-1.1.5.js"></script>
 <title>PT 결제창</title>
 </head>
 <body>
-<h1>PT 종류</h1>
-<button>10회 50 만원</button>
-<button>20회 90 만원</button>
-<button>30회 130 만원</button>
-<button>50회 200 만원</button>
-
+	<h1>PT 종류</h1>
+	<button type="button" id="pt10" value="10">10회 5 만원</button>
+	</br>
+	<button type="button" id="pt20"  value="20">20회 9 만원</button>
+	</br>
+	<button type="button" id="pt30"  value="30">30회 13 만원</button>
+	</br>
+	<button type="button" id="pt50"  value="50">50회 20 만원</button>
+	</br>
+	<button type="button" id="btn-kakaopay">카카오페이 결제</button>
+	<script>
+		$("#btn-kakaopay").click(function () {
+			var IMP = window.IMP;
+		IMP.init('imp12720430'); //가맹점식별코드
+			IMP.request_pay({
+				pg: 'kakaopay',
+				pay_method: 'card',
+				merchant_uid: 'merchant_' + new Date().getTime(),
+				name: '주문명 : pt 회원권 결제',
+				amount: 5000,
+				buyer_name: '테스트',
+				buyer_postcode: '123-456',
+				}, function (rsp) {
+					console.log(rsp);
+				if (rsp.success) {
+					var msg = '결제가 완료되었습니다.';
+					msg += '결제 금액 : ' + rsp.paid_amount;
+					// success.submit();.
+				} else {
+					var msg = '결제에 실패하였습니다.';
+					msg += '에러내용 : ' + rsp.error_msg;
+				}
+				alert(msg);
+			});
+		});
+	</script>
 </body>
 </html>
